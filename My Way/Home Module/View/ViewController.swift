30ef195ed5b3cede8cc7e18b0143d0100b87bb90
zein rezky chandra <zeinrezkychandra@zeins-MacBook-Pro.local>
2020-07-15 10:35:03 +0700

    initial commit

---
+++ My Way/Home Module/View/ViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ViewController.swift
+//  My Way
+//
+//  Created by zein rezky chandra on 08/10/18.
+//  Copyright © 2018 Zein. All rights reserved.
+//
+
+import UIKit
+
+protocol PositionValueDelegate {
+    func onValueChanged() -> ()
+}
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var sliderTop: UISlider!
+    @IBOutlet weak var sliderMiddle: UISlider!
+    @IBOutlet weak var sliderBottom: UISlider!
+    
+    var presenter: HomeViewPresenter?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        loadCurrentContext()
+    }
+        
+    func saveCurrentContext(rgb: (CGFloat, CGFloat, CGFloat)) -> (Void){
+        presenter?.onValueChange(rgb: rgb)
+    }
+    
+    func loadCurrentContext() -> () {
+        presenter?.onLoadCurrentColor()
+    }
+    
+    @IBAction func redValueChanged(_ sender: UISlider) {
+        print("red value : \(sender.value)")
+        // FIXME: Complete me so the value changed will affect the UI, by simply activate the breakpoint on the left
+    }
+    @IBAction func greenValueChanged(_ sender: UISlider) {
+        print("green value : \(sender.value)")
+        // FIXME: Complete me so the value changed will affect the UI, by simply activate the breakpoint on the left
+    }
+    @IBAction func blueValueChanged(_ sender: UISlider) {
+        print("blue value : \(sender.value)")
+        // FIXME: Complete me so the value changed will affect the UI, by simply activate the breakpoint on the left
+    }
+
+}
+
+extension HomeViewController: HomeView {
+    func loadCurrentContext(rgb: (CGFloat, CGFloat, CGFloat)) {
+        DispatchQueue.main.async {
+            self.sliderTop.value = Float(rgb.0)
+            self.sliderMiddle.value = Float(rgb.1)
+            self.sliderBottom.value = Float(rgb.2)
+            self.myLabel.text = "RGB ( \(rgb.0), \(rgb.1), \(rgb.2) )"
+        }
+        DispatchQueue.global(qos: .default).async {
+            DispatchQueue.main.async {
+                self.view.backgroundColor = UIColor(red: rgb.0/255, green: rgb.1/255, blue: rgb.2/255, alpha: 1.0)
+            }
+        }
+    }
+}
+
+extension HomeViewController: PositionValueDelegate{
+    func onValueChanged() {
+        let divider: Float = 1
+
+        let x = CGFloat(round(sliderTop.value / divider) * divider)
+        let y = CGFloat(round(sliderMiddle.value / divider) * divider)
+        let z = CGFloat(round(sliderBottom.value / divider) * divider)
+
+        DispatchQueue.main.async {
+            self.myLabel.text = "RGB ( \(x), \(y), \(z) )"
+            self.view.backgroundColor = UIColor(red: x/255, green: y/255, blue: z/255, alpha: 1.0)
+        }
+        saveCurrentContext(rgb: (x, y, z))
+    }
+}
+
